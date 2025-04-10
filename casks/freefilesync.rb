6dd369f2f0aec07971174f7f5c789ec6ac4dd2a7
zazzeron <zincs.orbiter_7y@icloud.com>
2025-04-10 06:15:32 -0300

    url update

--- casks/freefilesync.rb
+++ casks/freefilesync.rb
@@ -2,7 +2,7 @@ cask "freefilesync" do
   version "latest"
   sha256 :no_check
 
-  url "https://github.com/hkneptune/FreeFileSync/releases/latest/download/FreeFileSync_macOS.dmg",
+  url "https://github.com/hkneptune/FreeFileSync/releases/download/v14.3/FreeFileSync_v14.3_macOS.dmg",
       verified: "github.com/hkneptune/FreeFileSync/"
   name "FreeFileSync"
   desc "Poderosa ferramenta de sincronização de arquivos"
