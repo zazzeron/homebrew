7ee82a701d35d6e9f5d991db1b2566a898e4e803
zazzeron <zincs.orbiter_7y@icloud.com>
2025-04-10 07:21:58 -0300

    Update freefilesync.rb

--- casks/freefilesync.rb
+++ casks/freefilesync.rb
@@ -1,17 +1,14 @@
 cask "freefilesync" do
   version "14.3"
-  sha256 :no_check
-
-  url "https://github.com/hkneptune/FreeFileSync/releases/download/v14.3/FreeFileSync_14.3_macOS.zip",
+  url "https://github.com/hkneptune/FreeFileSync/releases/download/v#{version}/FreeFileSync_#{version}_macOS.zip",
       verified: "github.com/hkneptune/FreeFileSync/"
   name "FreeFileSync"
   desc "Poderosa ferramenta de sincronização de arquivos"
   homepage "https://freefilesync.org/"
 
-  app "FreeFileSync_14.3.app"
+  auto_updates true
 
-  zap trash: [
-    "~/Library/Application Support/FreeFileSync",
-    "~/Library/Preferences/org.freefilesync.FreeFileSync.plist",
-  ]
+  pkg "FreeFileSync_#{version}.pkg"
+
+  uninstall pkgutil: "org.freefilesync.FreeFileSync"
 end
